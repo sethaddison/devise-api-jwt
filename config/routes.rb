f13df8673b4f00cfde4648e53de1c675a0606792
sethaddison <sethaddison@gmail.com>
2023-03-02 09:21:51 -0800

    Add rack session fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {
+  devise_for :users, 
+  defaults: { format: :json },
+  path: '',
+  path_names: {
     sign_in: 'login',
     sign_out: 'logout',
     registration: 'signup'
